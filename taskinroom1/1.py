feaf09ac6d8ea5d283e2e72523e826b0fb7fbaf0
polenom <vitalimitsevich@yandex.by>
2021-08-26 20:17:28 +0300

    19

--- taskinroom1/1.py
+++ taskinroom1/1.py
@@ -1,8 +1,6 @@
 N = int(input())
 if N%2 == 0:
     print('Число {0} четное'.format(N))
-elif N == 0:
-    pass
 else:
     print('Число {0} нечетное'.format(N))
 
--- taskinroom1/6.py
+++ taskinroom1/6.py
@@ -2,6 +2,8 @@ def sc(a,b,c):
     D=b**2-4*a*c
     if D < 0:
         return None,None
+    elif D == 0:
+        return -b/2*a
     x1=(-b+D**0.5)/2*a
     x2=(-b-D**0.5)/2*a
     return round(x1,2),round(x2,2)
