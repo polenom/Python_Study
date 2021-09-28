66d14bb41735987f5d691b9e1f49937575709ef9
polenom <vitalimitsevich@yandex.by>
2021-09-28 17:08:06 +0300

    task6.1

--- task6/5.py
+++ task6/5.py
@@ -1,6 +1,6 @@
 text = input().split()
 for i in range(1, len(text)):
     if (text[i][0] == '-' and text[i - 1][0] == '-') or (text[i][0] != '-' and text[i - 1][0] != '-'):
-        print(text[i-1], text[i])
+        print( text[i-1], text[i] )
         break
     
